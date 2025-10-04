ccbbb0819d8240c3a8d64b4ee8fa33b3b46760b3
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-05 02:00:56 +0530

    Update lem4bit.v
    stable
    doesnt stay dead

--- lem4/lem4bit.v
+++ lem4/lem4bit.v
@@ -10,37 +10,35 @@ module top_module(
     output aaah,
     output digging );
     parameter L=0, R=1;
-    reg [3:0]state, next_state;//{death,dig,fall,direction}
-    reg [7:0] count;
+    int count;
+    reg [3:0]state, next_state;
     always @(*) begin
-        if(next_state[1]==0)begin
-            if(count<20)begin
-                next_state[2]<=state[1]?0:(dig?1:state[2]);
-                if(next_state[2]==0)begin
-                    case(state)
-                        L:next_state[0] <= (bump_left?R:L);
-                        R:next_state[0] <= (bump_right?L:R);           
-                    endcase// State transition logic
-                end
-                else  next_state[0] <= state[0];
+      next_state[3]=~(count<20);      
+         if(next_state[1]==0)begin
+            next_state[2]<=state[1]?0:(dig?1:state[2]);
+            if(next_state[2]==0)begin
+                case(state)
+                    L:next_state[0] <= (bump_left?R:L);
+                    R:next_state[0] <= (bump_right?L:R);           
+                endcase// State transition logic
             end
-            else next_state <= 4'b1000;
+            else  next_state[0] <= state[0];
         end
-        else begin            
+        else begin
             next_state[0] <= state[0];
             next_state[2] <= 0 ;  
         end
         next_state[1]<=~ground;
     end
 
-    always @(posedge clk or posedge areset) begin
-        count = areset?0:(ground?0:count+1);
+    always @(posedge clk, posedge areset) begin
+      count = areset?0:(ground?0:count+1);
         state = areset?L:next_state;// State flip-flops with asynchronous reset
     end
 
     // Output logic
     assign walk_left = (state==L);
     assign walk_right = (state==R);
-    assign aaah = (state[1]==1'b1)&(~state[3]);
+    assign aaah = (state[1])&(~state[3]);
     assign digging = state[2]&(~state[3]);
 endmodule
