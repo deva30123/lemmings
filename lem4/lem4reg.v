69acffba934ac296972732a71bd2cf7d15c5921a
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-05 01:22:03 +0530

    Update lem4reg.v

--- lem4/lem4reg.v
+++ lem4/lem4reg.v
@@ -9,8 +9,8 @@ module top_module(
     output walk_right,
     output aaah,
     output digging );   
-    reg [7:0] count;
-     parameter L=0, R=1,LF=2,RF=3,DL=4,DR=5,X=6;
+    int count ;
+     parameter L=0, R=1,LF=2,RF=3,DL=4,DR=5,X=8;
     reg [3:0]state, next_state;//{death,dig,fall,direction}
     always @(*) begin       
             case(state)
@@ -23,8 +23,9 @@ module top_module(
                 X:next_state = X;
             endcase// State transition logic 
     end
-    always @(posedge clk, posedge areset) begin
-        count = areset?0:(ground?0:count+1);
+    always @(posedge clk or posedge areset) begin
+        count = areset?0:(ground?0:(count+1));
+    	//count = count+1;
         state = areset?L:next_state;// State flip-flops with asynchronous reset
     end
     // Output logic
